2f23f581a6743c39bf94679941b09f41fcf5e9c7
Xiulin Gao <xiulin.gao.edu@gmail.com>
2025-07-23 10:58:01 -0700

    scripts from lesson 1

---
+++ exe13.py
@@ -0,0 +1,9 @@
+from sys import argv
+script, first, second, third = argv
+print("Your script is:", script)
+print("Your first argument is:", first)
+print("Your second argument is:", second)
+print("Your third argument is:", third)
+age = input("Your age: ")
+height = input("Your height: ")
+print(f"So you are {age} years old and {height} cm tall.")
